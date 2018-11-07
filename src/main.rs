b7a3ec069070c4c6527bb7d0985dd15bdc7304ce
Joel Colledge <joel.colledge@gmail.com>
2018-11-07 21:26:31 +0100

    Adds partition filling last free region

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,73 @@ use std::process::Command;
 use std::str;
 
 fn main() {
-    println!("Hello, world! local repo");
+    println!("Setting up mass storage volume");
 
-    let output = Command::new("parted")
-        .arg("--script")
-        .arg("--machine")
-        .arg("/dev/mmcblk0")
-        .arg("unit").arg("MB")
-        .arg("print").arg("free")
+    println!("Getting SD partitions");
+    let parted_output = command_stdout(
+        Command::new("parted")
+            .arg("--script")
+            .arg("--machine")
+            .arg("/dev/mmcblk0")
+            .arg("unit").arg("MB")
+            .arg("print").arg("free")
+    );
+
+    let (from_str, to_str) = parted_find_last_free(parted_output.as_str());
+
+    println!("Adding SD partition");
+    command_stdout(
+        Command::new("parted")
+            .arg("--script")
+            .arg("/dev/mmcblk0")
+            .arg("mkpart").arg("primary").arg("").arg(from_str).arg(to_str)
+    );
+}
+
+fn command_stdout(command: &mut Command) -> String {
+    let output = command
         .output()
-        .expect("failed to execute process");
+        .expect("Failed to execute process");
 
-    println!("stderr debug: {:?}", &output.stderr);
-    println!("stderr: {}", str::from_utf8(&output.stderr).expect("failed to parse stderr"));
+    if !output.status.success() {
+        match output.status.code() {
+            Some(code) => panic!("External process exited with status code: {}", code),
+            None => panic!("External process terminated by signal")
+        };
+    };
 
-    println!("stdout debug: {:?}", &output.stdout);
-    println!("stdout: {}", str::from_utf8(&output.stdout).expect("failed to parse stdout"));
+    String::from_utf8(output.stdout).expect("failed to parse stdout")
+}
+
+fn parted_find_last_free(parted_output: &str) -> (&str, &str) {
+    let free_line = parted_output.lines()
+        .filter(|line| line.ends_with("free;"))
+        .last()
+        .expect("no 'free' lines in parted output");
+
+    let free_line_parts: Vec<&str> = free_line.split(":").collect();
+
+    (
+        free_line_parts[1],
+        free_line_parts[2]
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parted_find_last_free() {
+        let (from, to) = parted_find_last_free("
+            BYT;
+            /dev/mmcblk0:31915MB:sd/mmc:512:512:msdos:SD ACLCD:;
+            1:0.02MB:4.19MB:4.18MB:free;
+            1:4.19MB:46.1MB:41.9MB:fat16::boot, lba;
+            2:46.1MB:201MB:155MB:ext3::;
+            1:201MB:31915MB:31714MB:free;
+        ");
+        assert_eq!(from, "201MB");
+        assert_eq!(to, "31915MB");
+    }
 }
